c54eec2d7713b1ffe928c53767ebbb5575bd5ed7
Lock <myselfon@gmail.com>
2016-11-06 02:02:02 +0800

    nothing to commit

--- new.py
+++ new.py
@@ -1,14 +1,10 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2016-11-06 00:42:19
+# @Date    : 2016-11-06 01:49:35
 # @Author  : Lock (myselfon@gmailcom)
 # @Link    : https://github.com/kadise/Python_Learn.git
 # @Version : $Id$
-# @Comments: Add from github ,add from samlime
 
 import os
 
-
-
-str = "abs"
-print(str + "world")
-print("github.com")
+print("main")
