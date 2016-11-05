ab7de73c9c0725725ed673628bc1c015d680055c
Lock <myselfon@gmail.com>
2016-11-06 00:48:50 +0800

    try

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-11-06 00:42:19
+# @Author  : Lock (myselfon@gmailcom)
+# @Link    : https://github.com/kadise/Python_Learn.git
+# @Version : $Id$
+
+import os
+
+str = "hello"
+print(str + "world")
+print("github.com")
